1028c0e36f73c00491b6b2d91d074e00f36b75b6
Anurag <anuragsawant@duck.com>
2025-07-02 22:14:31 +0530

    refactor: enhance vite.config.ts to load environment variables dynamically for base and port configuration

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,15 +1,16 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig(() => {
-  const base = process.env.VITE_BASENAME;
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  const base = env.VITE_BASENAME || "/";
 
   return {
     base,
     plugins: [react(), tailwindcss()],
     server: {
-      port: parseInt(process.env.PORT || "11001", 10),
+      port: parseInt(env.PORT || "11001", 10),
     },
   };
 });
