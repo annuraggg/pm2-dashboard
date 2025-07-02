020525b909371acdc4b2bf2246482325447190af
Anurag <anuragsawant@duck.com>
2025-07-02 21:57:24 +0530

    feat: add @types/node to devDependencies, remove unused SVG files, and refactor main.tsx structure

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,18 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { AuthProvider } from "./hooks/useAuth";
 import App from "./App";
-import "./index.css";
-import { Toaster } from "sonner";
+import { BrowserRouter } from "react-router-dom";
+
+const basename = import.meta.env.VITE_BASENAME || "";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-        <Toaster richColors />
-      </AuthProvider>
+    <BrowserRouter basename={basename}>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
