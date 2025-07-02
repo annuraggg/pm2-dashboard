b7912dc330ff40cb5d4838030323de285dda4c40
Anurag <anuragsawant@duck.com>
2025-07-02 22:10:54 +0530

    refactor: update vite.config.ts to use a function for dynamic base and port configuration

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,10 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    port: parseInt(process.env.PORT || "5173", 10), // Default to 5173 if PORT is not set
-  },
+export default defineConfig(() => {
+  const base = process.env.VITE_BASENAME;
+
+  return {
+    base,
+    plugins: [react(), tailwindcss()],
+    server: {
+      port: parseInt(process.env.PORT || "11001", 10),
+    },
+  };
 });
