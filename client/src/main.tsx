57f53d2815cd47d016b48e4b75fcdffcfd3aee7d
Anurag <anuragsawant@duck.com>
2025-07-02 22:07:16 +0530

    feat: wrap App component with AuthProvider and add Toaster for notifications

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,14 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./hooks/useAuth";
+import App from "./App";
+import "./index.css";
+import { Toaster } from "sonner";
 
 const basename = import.meta.env.VITE_BASENAME || "";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter basename={basename}>
-      <App />
+      <AuthProvider>
+        <App />
+        <Toaster richColors />
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
